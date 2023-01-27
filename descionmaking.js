e37bcc08f146adf2e6c55f86eaebaf8e9d317df9
unknown <rinturose06@gmail.com>
2023-01-27 16:43:01 +0530

    intial commit

---
+++ descionmaking.js
@@ -0,0 +1,24 @@
+// var num =10;
+// if(num<=0){
+//     console.log("number 10 is -ve");
+// }
+// else{
+//     console.log("number 10 is +ve");
+// }
+
+
+// var num =10;
+// if(num<=0){
+//     console.log("number", num, "is -ve");   
+// } 
+// else{
+//     console.log("number", num," is +ve");
+// }
+
+var num =10;
+if(num<=0){
+     console.log(`number ${num} is +ve`);  
+}
+else{
+    console.log(`number ${num} is +ve`);
+}
---
+++ ifelseif.js
@@ -0,0 +1,11 @@
+num=0;
+if(num<0){
+    console.log(`number ${num} is +ve`);
+}
+else if(num>0){
+    console.log(`number ${num} is -ve`);
+
+}
+else{
+    console.log(`number ${num} is zero`);
+}
